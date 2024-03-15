191a626c63cd35a53f694d8cba9a7d820a52432b
m5xed <fieldhousehold04@gmail.com>
2024-03-15 01:16:20 +0000

    Version 1.3.1 - Added text input field for data

--- Bubble Sort/src/TopMenu.java
+++ Bubble Sort/src/TopMenu.java
@@ -10,9 +10,19 @@ public class TopMenu extends JFrame {
         title.setFont(new Font("Arial", Font.BOLD, 20));
         title.setForeground(Color.BLACK);
 
-
         JPanel menuContainer = new JPanel();
         String[] ALGORITHMS = {"Bubble Sort", "Placeholder", "Placeholder"};
+
+        JPanel inputContainer = new JPanel();
+
+        JLabel inputMessage = new JLabel("Input the unsorted data, seperated by commas:");
+        inputContainer.add(inputMessage);
+
+        // Add input for array data
+        JTextArea inputArea = new JTextArea(10, 18);
+        inputArea.setLineWrap(true);
+        inputContainer.add(inputArea);
+
         // Add combo Box
         JComboBox<String> algorithmSelection = new JComboBox<>(ALGORITHMS);
         menuContainer.add(algorithmSelection);
@@ -30,7 +40,8 @@ public class TopMenu extends JFrame {
 
         setLayout(new BorderLayout());
         add(title, BorderLayout.NORTH);
-        add(menuContainer, BorderLayout.CENTER);
+        add(inputContainer, BorderLayout.CENTER);
+        add(menuContainer, BorderLayout.SOUTH);
 
         // Set properties of main frame
         setSize(600, 400);
